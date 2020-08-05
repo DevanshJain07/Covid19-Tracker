de62b54a39daf5c64637a09e993d8c83fcbd0434
devansh071999 <devanshsampatjain@gmail.com>
2020-08-05 18:02:38 +0530

    Updates

---
+++ precache-manifest.9ceef7d83e70a45e6cf24b161a723131.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ebfbff3147c2119bf214ffc9feffb4b",
+    "url": "/Covid19-Tracker/index.html"
+  },
+  {
+    "revision": "26bdcfb8560b1309cdfa",
+    "url": "/Covid19-Tracker/static/css/main.d2d3349c.chunk.css"
+  },
+  {
+    "revision": "100f1a103a4389f34455",
+    "url": "/Covid19-Tracker/static/js/2.1c159eba.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/Covid19-Tracker/static/js/2.1c159eba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26bdcfb8560b1309cdfa",
+    "url": "/Covid19-Tracker/static/js/main.6091cd15.chunk.js"
+  },
+  {
+    "revision": "b3d327f1dc4abb6bdd8c",
+    "url": "/Covid19-Tracker/static/js/runtime-main.de293e09.js"
+  }
+]);
